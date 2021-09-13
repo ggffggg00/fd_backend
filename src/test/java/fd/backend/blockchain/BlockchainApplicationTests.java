8378dad0f1cac869348362d59516182f7f4037d3
zakhlevnynm <n.m.zakhlevny@novotelecom.ru>
2021-09-13 18:20:38 +0700

    init commit

---
+++ src/test/java/fd/backend/blockchain/BlockchainApplicationTests.java
@@ -0,0 +1,13 @@
+package fd.backend.blockchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
