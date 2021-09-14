c053105b6a7935a5c9ad6701c0ec8fc66c7931ed
zakhlevnynm <n.m.zakhlevny@novotelecom.ru>
2021-09-14 15:03:08 +0700

    add package

---
+++ src/main/java/fd/backend/blockchain/config/AppDbConfig.java
@@ -0,0 +1,9 @@
+package fd.backend.blockchain.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class AppDbConfig {
+}
